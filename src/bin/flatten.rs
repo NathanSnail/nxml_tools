51408e24ac35c4c1d6a982e8e64f21978deee14b
NathanSnail <nathan.titirangi@gmail.com>
2024-02-27 09:34:22 +1300

    get it to build

--- src/bin/flatten.rs
+++ src/bin/flatten.rs
@@ -1,6 +1,9 @@
 use mlua;
+use std::io::Read;
 
 fn main() {
+    let mut content: String = String::new();
+    std::fs::File::open("./nxml.lua").unwrap().read_to_string(&mut content).unwrap();
     let lua = mlua::Lua::new();
     let _ = lua.load("print('hi')").exec();
     println!("hi_rs!");
